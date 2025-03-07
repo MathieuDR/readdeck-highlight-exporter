d0d58cf09bcbf3a46fab4be3163467126350330c
MathieuDR <mathieu@deraedt.dev>
2025-03-07 15:33:41 +0100

    🚧 Create commands and add learnings in config

---
+++ cmd/config.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	// "fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Generate or view your configuration",
+  // LEARNING: Cannot escape ` in raw strings. [link](https://github.com/golang/go/issues/24475)
+  // LEARNING: You need to have a `,` at the end, even if it's the last thing in this object/struct/class(?)
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
