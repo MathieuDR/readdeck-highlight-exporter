03cd456d5579bb6e9748c1191facabe24b70f483
MathieuDR <mathieu@deraedt.dev>
2025-04-05 02:19:00 +0200

    ♻️ Refactor file repo

--- internal/repository/note_service.go
+++ internal/repository/note_service.go
@@ -1,41 +1,58 @@
+// internal/repository/note_service.go
 package repository
 
 import (
 	"github.com/mathieudr/readdeck-highlight-exporter/internal/model"
 )
 
-type NoteService struct {
+// NoteService combines all note operations into a single interface
+type NoteService interface {
+	ParseNote(content []byte, path string) (model.ParsedNote, error)
+	GenerateNoteContent(note model.Note) (NoteOperation, error)
+	UpdateNoteContent(existing model.ParsedNote, note model.Note) (NoteOperation, error)
+}
+
+type ComprehensiveNoteService struct {
 	Parser    NoteParser
 	Generator NoteGenerator
 	Updater   NoteUpdater
 }
 
-func NewNoteService() *NoteService {
+func NewNoteService() NoteService {
 	formatter := NewHighlightFormatter(DefaultColorConfig())
 	parser := NewYAMLNoteParser()
 	generator := NewYAMLNoteGenerator(formatter)
 	updater := NewYAMLNoteUpdater(generator, parser)
 
-	return &NoteService{
+	return &ComprehensiveNoteService{
 		Parser:    parser,
 		Generator: generator,
 		Updater:   updater,
 	}
 }
 
-// Implement all the required interfaces
-var _ NoteParser = (*NoteService)(nil)
-var _ NoteGenerator = (*NoteService)(nil)
-var _ NoteUpdater = (*NoteService)(nil)
+func NewCustomNoteService(parser NoteParser, generator NoteGenerator, updater NoteUpdater) NoteService {
+	return &ComprehensiveNoteService{
+		Parser:    parser,
+		Generator: generator,
+		Updater:   updater,
+	}
+}
 
-func (s *NoteService) ParseNote(content []byte, path string) (model.ParsedNote, error) {
+var _ NoteService = (*ComprehensiveNoteService)(nil)
+var _ NoteParser = (*ComprehensiveNoteService)(nil)
+var _ NoteGenerator = (*ComprehensiveNoteService)(nil)
+var _ NoteUpdater = (*ComprehensiveNoteService)(nil)
+
+func (s *ComprehensiveNoteService) ParseNote(content []byte, path string) (model.ParsedNote, error) {
 	return s.Parser.ParseNote(content, path)
 }
 
-func (s *NoteService) GenerateNoteContent(note model.Note) (NoteOperation, error) {
+func (s *ComprehensiveNoteService) GenerateNoteContent(note model.Note) (NoteOperation, error) {
 	return s.Generator.GenerateNoteContent(note)
 }
 
-func (s *NoteService) UpdateNoteContent(existing model.ParsedNote, note model.Note) (NoteOperation, error) {
+func (s *ComprehensiveNoteService) UpdateNoteContent(existing model.ParsedNote, note model.Note) (NoteOperation, error) {
 	return s.Updater.UpdateNoteContent(existing, note)
 }
+
